3db17cfccb17733c82543c779a31445ee6eae736
Ansh Chouksey <anshchouksey08@gmail.com>
2024-09-17 17:45:20 +0530

    Create Minimize the Heights II.cpp

---
+++ Minimize the Heights II.cpp
@@ -0,0 +1,51 @@
+ class Solution {
+  public:
+    int getMinDiff(vector<int> &arr, int k) {
+        int n = arr.size();
+        vector<pair<int, int>> v;
+        vector<int> taken(n);
+
+         for (int i = 0; i < n; i++) {
+            if (arr[i] - k >= 0) {
+                v.push_back({arr[i] - k, i});
+            }
+            v.push_back({arr[i] + k, i});
+        }
+        sort(v.begin(), v.end());
+        int elements_in_range = 0;
+        int left = 0;
+        int right = 0;
+
+      
+        while (elements_in_range < n && right < v.size()) {
+            if (taken[v[right].second] == 0) {
+                elements_in_range++;
+            }
+            taken[v[right].second]++;
+            right++;
+        }
+        int ans = v[right - 1].first - v[left].first;
+        while (right < v.size()) {
+            if (taken[v[left].second] == 1) {
+                elements_in_range--;
+            }
+            taken[v[left].second]--;
+            left++;
+
+            while (elements_in_range < n && right < v.size()) {
+                if (taken[v[right].second] == 0) {
+                    elements_in_range++;
+                }
+                taken[v[right].second]++;
+                right++;
+            }
+
+            if (elements_in_range == n) {
+                ans = min(ans, v[right - 1].first - v[left].first);
+            } else {
+                break;
+            }
+        }
+        return ans;
+    }
+};

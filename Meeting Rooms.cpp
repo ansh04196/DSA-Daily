2f4a74fac6207a33328c1f224937ba455c188478
Ansh Chouksey <anshchouksey08@gmail.com>
2024-11-12 01:24:09 +0530

    Create Meeting Rooms.cpp

---
+++ Meeting Rooms.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    bool canAttend(vector<vector<int>> &arr) {
+        // Your Code Here
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<arr.size()-1;i++){
+            if(arr[i][1]>arr[i+1][0])
+                return false;
+        }
+        return true;
+    }
+};

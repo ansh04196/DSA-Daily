8a394ba0bea638a547e4c57719c335f1bd3f0ae2
Ansh Chouksey <anshchouksey08@gmail.com>
2025-03-25 15:40:53 +0530

    Create Check if Grid can be Cut into Sections.cpp

---
+++ Check if Grid can be Cut into Sections.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    bool checker(vector<vector<int>>& rectangles, int dim) {
+        int gapCount = 0;
+        sort(rectangles.begin(), rectangles.end(),
+             [dim](const vector<int>& a, const vector<int>& b) {
+                 return a[dim] < b[dim];
+             });
+        int furthestEnd = rectangles[0][dim + 2];
+        for (size_t i = 1; i < rectangles.size(); i++) {
+            vector<int>& rect = rectangles[i];
+            if (furthestEnd <= rect[dim]) gapCount++;
+            furthestEnd = max(furthestEnd, rect[dim + 2]);
+        }
+        return gapCount >= 2;
+    }
+public:
+    bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
+        return checker(rectangles, 0) || checker(rectangles, 1);
+    }
+};

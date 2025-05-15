0866f26a38533802b7ff96c5552d537c92fa4eeb
Ansh Chouksey <anshchouksey08@gmail.com>
2025-05-15 10:17:27 +0530

    Create Substrings with same first and last characters.cpp

---
+++ Substrings with same first and last characters.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+  public:
+    int countSubstring(string &s) {
+        // code here
+        vector<int> freq(26, 0);
+        for (char ch : s)
+            freq[ch - 'a']++;
+        int count = 0;
+        for (int f : freq)
+            count += (f * (f + 1)) / 2;
+        return count;
+    }
+};
+
+
+//GFG POTD solution for 15 May

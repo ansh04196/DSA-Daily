0d5a2ffee92d0a499c87a38d2bd671c8ff38dbba
Ansh Chouksey <anshchouksey08@gmail.com>
2025-11-15 11:18:00 +0530

    Add minCutCost function for stick cutting problem
    Implement dynamic programming solution to calculate minimum cost for cutting a stick given its length and cut positions.

---
+++ Minimum Cost to Cut a Stick of length N.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int minCutCost(int n, vector<int>& cuts) {
+        cuts.push_back(0);
+        cuts.push_back(n);
+        sort(cuts.begin(), cuts.end());
+        int m = cuts.size();  
+        vector<vector<int>> dp(m, vector<int>(m, 0));
+        for(int len = 2; len < m; len++) {           // interval length
+            for(int i = 0; i + len < m; i++) {
+                int j = i + len;
+                dp[i][j] = INT_MAX;
+                for(int k = i + 1; k < j; k++) {     // try every mid cut
+                    int cost = (cuts[j] - cuts[i]) + dp[i][k] + dp[k][j];
+                    dp[i][j] = min(dp[i][j], cost);
+                }
+                if(dp[i][j] == INT_MAX) dp[i][j] = 0; // no cuts in interval
+            }
+        }
+        return dp[0][m-1];
+    }
+};
+
+//GFG POTD solution for 15 November
